13a6ba993dd97f7a0cae7c052c2faf1fef28602c
lkhuge <lkwinghu@gmail.com>
2014-04-16 19:47:16 +0800

    [Change]完善代码

--- Wing.WeiXin.MP.SDK/Enumeration/ReturnEntityType.cs
+++ Wing.WeiXin.MP.SDK/Enumeration/ReturnEntityType.cs
@@ -10,5 +10,34 @@ namespace Wing.WeiXin.MP.SDK.Enumeration
     /// </summary>
     public enum ReturnEntityType
     {
+        /// <summary>
+        /// 回复图片消息
+        /// </summary>
+        ReturnMessageImage,
+
+        /// <summary>
+        /// 回复音乐消息
+        /// </summary>
+        ReturnMessageMusic,
+
+        /// <summary>
+        /// 回复图文消息
+        /// </summary>
+        ReturnMessageNews,
+
+        /// <summary>
+        /// 回复文本消息
+        /// </summary>
+        ReturnMessageText,
+
+        /// <summary>
+        /// 回复视频消息
+        /// </summary>
+        ReturnMessageVideo,
+
+        /// <summary>
+        /// 回复语音消息
+        /// </summary>
+        ReturnMessageVoice
     }
 }
