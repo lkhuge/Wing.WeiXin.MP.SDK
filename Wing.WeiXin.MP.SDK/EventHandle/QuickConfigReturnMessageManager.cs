6ddb0dd27660968e9586beea62b8b1a9569e677e
lkhuge <lkwinghu@gmail.com>
2014-04-16 19:44:28 +0800

    [Add]添加快速配置回复消息模块

---
+++ Wing.WeiXin.MP.SDK/EventHandle/QuickConfigReturnMessageManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wing.WeiXin.MP.SDK.Entities.ReceiveMessages.Messages;
+using Wing.WeiXin.MP.SDK.Entities.ReturnMessages;
+using Wing.WeiXin.MP.SDK.Lib.FileManager;
+
+namespace Wing.WeiXin.MP.SDK.EventHandle
+{
+    /// <summary>
+    /// 快速配置回复消息管理类
+    /// </summary>
+    public static class QuickConfigReturnMessageManager
+    {
+        #region 获取回复文本消息的快速配置事件 public static EntityHandler.CustomEntityHandler<MessageText> GetQuickConfigEntityHandler(string fileName, string str)
+        /// <summary>
+        /// 获取回复文本消息的快速配置事件
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <param name="str">匹配字符串</param>
+        /// <returns>回复文本消息的快速配置事件</returns>
+        public static EntityHandler.CustomEntityHandler<MessageText>
+            GetQuickConfigEntityHandler(string fileName, string str)
+        {
+            Dictionary<string, string> quickConfig = new Dictionary<string, string>
+            {
+                {"Content", ""},
+            };
+            return messageText =>
+            {
+                FileHelper.ReadOfKeyValueData(fileName, quickConfig);
+                return str.Equals(messageText.Content)
+                    ? new ReturnMessageText(quickConfig["Content"], messageText)
+                    : null;
+            };
+        }
+        #endregion
+    }
+}
---
+++ Wing.WeiXin.MP.SDK/Lib/FileManager/FileHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Wing.WeiXin.MP.SDK.Exception;
+
+namespace Wing.WeiXin.MP.SDK.Lib.FileManager
+{
+    /// <summary>
+    /// 文件工具类
+    /// </summary>
+    public static class FileHelper
+    {
+        #region 从文件中读取KeyValue数据 public static Dictionary<string, string> ReadOfKeyValueData(string fileName, Dictionary<string, string> data = null)
+        /// <summary>
+        /// 从文件中读取KeyValue数据
+        /// </summary>
+        /// <param name="fileName">文件名称</param>
+        /// <param name="data">填充的KeyValue数据列表</param>
+        /// <returns>KeyValue数据</returns>
+        public static Dictionary<string, string> ReadOfKeyValueData(string fileName, Dictionary<string, string> data = null)
+        {
+            try
+            {
+                Dictionary<string, string> kvList = data ?? new Dictionary<string, string>();
+                using (StreamReader srReadFile = new StreamReader(fileName))
+                {
+                    while (!srReadFile.EndOfStream)
+                    {
+                        string strReadLine = srReadFile.ReadLine();
+                        if (String.IsNullOrEmpty(strReadLine)
+                            || String.IsNullOrEmpty(strReadLine.Trim())) continue;
+                        string[] strList = strReadLine.Split(':');
+                        if (strList.Length != 2) continue;
+                        string kvKey = strList[0].Trim();
+                        string kvValue = strList[1].Trim();
+                        if (data != null && !kvList.ContainsKey(kvKey)) continue;
+                        kvList[kvKey] = kvValue;
+                    }
+                }
+                return kvList;
+            }
+            catch (FileNotFoundException fileNotFound)
+            {
+                throw new WXException("KeyValue数据文件未找到", fileNotFound);
+            }
+            catch (DirectoryNotFoundException directoryNotFound)
+            {
+                throw new WXException("KeyValue数据文件未找到", directoryNotFound);
+            }
+            catch (IOException io)
+            {
+                throw new WXException("读取KeyValue数据错误", io);
+            }
+        }
+        #endregion
+    }
+}
