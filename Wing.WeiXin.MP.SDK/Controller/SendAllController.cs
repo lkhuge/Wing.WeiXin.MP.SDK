76249d7cbd5f37fe5ec16b72fd169d7b0e1ba271
lkhuge <lkwinghu@gmail.com>
2014-04-19 15:48:59 +0800

    [Add]添加群发控制器

---
+++ Wing.WeiXin.MP.SDK/Controller/SendAllController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wing.WeiXin.MP.SDK.Controller
+{
+    /// <summary>
+    /// 群发控制器
+    /// </summary>
+    public static class SendAllController
+    {
+        
+    }
+}
