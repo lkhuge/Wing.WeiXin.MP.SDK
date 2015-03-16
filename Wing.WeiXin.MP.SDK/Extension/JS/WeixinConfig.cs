e7588bba26cfeae239551da3b974fe8f816c7e9b
lkhuge <lkwinghu@gmail.com>
2015-03-16 22:21:07 +0800

    <2.2.3.1>
    [Add]添加微信配置接口以及对应的JS类库

---
+++ Wing.WeiXin.MP.SDK/Extension/JS/WeixinConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Wing.WeiXin.MP.SDK.Controller;
+using Wing.WeiXin.MP.SDK.Entities;
+using Wing.WeiXin.MP.SDK.Lib;
+using Wing.WeiXin.MP.SDK.Properties;
+
+namespace Wing.WeiXin.MP.SDK.Extension.JS
+{
+    /// <summary>
+    /// 微信配置接口
+    /// </summary>
+    public class WeixinConfig : IHttpHandler
+    {
+        /// <summary>
+        /// 微信账号
+        /// 如果为空则默认为第一个账号
+        /// </summary>
+        public static WXAccount Account;
+
+        /// <summary>
+        /// 是否使用调试模式
+        /// </summary>
+        public static bool IsDebug;
+        
+        /// <summary>
+        /// URL参数名称
+        /// </summary>
+        public static string URL = "url";
+
+        /// <summary>
+        /// 接口列表参数名称
+        /// </summary>
+        public static string APIList = "apiList";
+
+        /// <summary>
+        /// JSSDK控制器
+        /// </summary>
+        private readonly JSController jsController = new JSController();
+
+        #region 响应事件 public void ProcessRequest(HttpContext context)
+        /// <summary>
+        /// 响应事件
+        /// </summary>
+        /// <param name="context">上下文</param>
+        /// <returns>响应结果</returns>
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write(JSONHelper.JSONSerialize(jsController.GetJSWeixinConfig(
+                Account ?? GlobalManager.GetFirstAccount(),
+                HttpUtility.UrlDecode(context.Request.QueryString[URL]),
+                context.Request.QueryString[APIList].Split(','),
+                IsDebug)));
+        }
+        #endregion
+
+        /// <summary>
+        /// 是否重用
+        /// </summary>
+        public bool IsReusable { get; private set; }
+    }
+}
---
+++ Wing.WeiXin.MP.SDK/Scripts/wing.weixin.mp.js.sdk.js
@@ -0,0 +1,50 @@
+﻿/**
+ * Weixin JS SDK
+ *
+ * param url 获取参数接口的URL(不填默认是"/WeixinConfig")
+ */
+function weixin(url) {
+    if (typeof jQuery == 'undefined') {
+        alert('未发现JQuery');
+    }
+    /**
+     * 载入配置
+     *
+     * param apiList API列表
+     * param callback 获取成功后的回调方法
+     */
+    this.load = function (apiList, callback) {
+        //判断接口列表是否存在
+        if (!$.isArray(apiList) || apiList.length == 0) {
+            alert('API列表不能为空');
+        }
+
+        //获取配置
+        $.getJSON((typeof url == 'undefined' ? '/WeixinConfig' : url) +
+            '?url=' + encodeURIComponent((typeof this.href != 'undefined') ? this.href : location.href) +
+            '&apiList=' + apiList.join(), function (data) {
+            //注入配置
+            wx.config(data);
+
+            //配置注入完成后执行对应操作
+            wx.ready(function () {
+                //执行回调方法
+                callback();
+            });
+
+            //配置注入失败
+            wx.error(function (res) {
+                failCallback(res);
+            });
+        });
+    }
+
+    /**
+     * 获取失败后的回调方法
+     *
+     * param r 失败原因描述
+     */
+    this.failCallback = function (r) {
+        alert('配置注入失败，原因：' + r);
+    }
+}
